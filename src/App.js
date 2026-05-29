23bec144fa5063c36d8a9feff35815af004fdf95
jgozner <jgozner@apryse.com>
2026-05-29 12:37:22 -0700

    Added files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import WebViewer from '@pdftron/webviewer';
+
+function App() {
+  const viewer = useRef(null);
+  const [instance, setInstance] = useState(null);
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: '/files/WebviewerDemoDoc.1.pdf',
+        licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
+        fullAPI: true
+      },
+      viewer.current,
+    ).then((instance) => {
+      setInstance(instance);
+    });
+    }, []);
+
+    return (
+      <div className="App">
+        <div className="webviewer" ref={viewer}></div>
+      </div>
+    );
+  }
+
+export default App;
