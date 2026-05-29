29cc2f398746878eecce6554b132b39eebac22d5
jgozner <jgozner@apryse.com>
2026-05-29 12:55:32 -0700

    Added tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import WebViewer from '@pdftron/webviewer';
 
 function App() {
-  const viewer = useRef(null);
+  const viewer1 = useRef(null);
+  const viewer2 = useRef(null);
   const [instance, setInstance] = useState(null);
 
   useEffect(() => {
@@ -12,17 +15,37 @@ function App() {
         path: '/webviewer/lib',
         initialDoc: '/files/WebviewerDemoDoc.pdf',
         licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
-        fullAPI: true
       },
-      viewer.current,
+      viewer1.current,
     ).then((instance) => {
-      setInstance(instance);
+    });
+
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: '/files/Legal.docx',
+        enableOfficeEditing: true,
+        licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
+      },
+      viewer2.current,
+    ).then((instance) => {
     });
     }, []);
 
     return (
       <div className="App">
-        <div className="webviewer" ref={viewer}></div>
+        <Tabs forceRenderTabPanel={true}>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
+          <TabPanel>
+            <div className="webviewer" ref={viewer1}></div>
+          </TabPanel>
+          <TabPanel>
+            <div className="webviewer" ref={viewer2}></div>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
